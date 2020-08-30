f739d483ea40137a0035557abd480f28f30846d7
AnnaSimolchuk <annaadlerzh@gmail.com>
2020-08-30 21:15:41 +0300

    2

---
+++ module1/task.js
@@ -0,0 +1,25 @@
+// const employees = 5;
+// const minSalary = 500;
+// const maxSalary = 5000;
+// let totalSalary = 0;
+
+// for (let i = 1; i <= employees; i += 1) {
+//   const salary = Math.round(
+//     Math.random() * (maxSalary - minSalary) + minSalary
+//   );
+//   console.log(`Зп работника номер ${i} - ${salary}`);
+// }
+
+// const numbers = [25, 155, 52, 4, 36, 95];
+// let biggestNumber = numbers[0];
+
+// for (const number of numbers) {
+//   if (number > biggestNumber) {
+//     biggestNumber = number;
+//   }
+// }
+
+// console.log("biggestNumber ", biggestNumber);
+const scores = [1, 2, 3, 4, 5];
+
+[1, 2, 3, 4, 5].splice(2, 2, 6, 7);
