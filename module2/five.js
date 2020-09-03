fa4376589bf859bfabb2569257304fb12d26e813
AnnaSimolchuk <annaadlerzh@gmail.com>
2020-09-03 22:24:29 +0300

    2 модуль

---
+++ module2/five.js
@@ -0,0 +1,15 @@
+function checkForSpam(str) {
+  "use strict";
+  // Write code under this line
+  const string = str.toLowerCase();
+
+  return string.includes("spam") || string.includes("sale") ? true : false;
+}
+
+//console.log(checkForSpam('Latest technology news')); // false
+
+//console.log(checkForSpam('JavaScript weekly newsletter')); // false
+
+//console.log(checkForSpam('Get best sale offers now!')); // true
+
+//console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
